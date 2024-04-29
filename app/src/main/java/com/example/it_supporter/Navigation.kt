b8c78a3c05d5a598e478c934a3ca7e3a6c5626f4
marcus_rk <makj0005@stud.kea.dk>
2024-04-29 10:39:06 +0200

    Made dialog box

--- app/src/main/java/com/example/it_supporter/Navigation.kt
+++ app/src/main/java/com/example/it_supporter/Navigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.it_supporter.ui.theme.screens.DescriptionScreen
 import com.example.it_supporter.ui.theme.screens.Device
 import com.example.it_supporter.ui.theme.screens.WelcomeScreen
 
