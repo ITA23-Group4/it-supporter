ae4de2133f60e573da1751085ef79d7b33c2cc77
marcus_rk <makj0005@stud.kea.dk>
2024-04-29 09:54:52 +0200

    added more to Column

--- app/src/main/java/com/example/it_supporter/ui/theme/components/CustomColumn.kt
+++ app/src/main/java/com/example/it_supporter/ui/theme/components/CustomColumn.kt
@@ -1,6 +1,7 @@
 package com.example.it_supporter.ui.theme.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,7 +12,9 @@ fun CustomColumn(
     modifier: Modifier = Modifier
 ) {
     Column (
-        modifier.padding(30.dp, 0.dp)
+        modifier
+            .padding(30.dp, 0.dp)
+            .fillMaxSize()
     ){
     }
 }
--- app/src/main/java/com/example/it_supporter/ui/theme/screens/Welcome.kt
+++ app/src/main/java/com/example/it_supporter/ui/theme/screens/Welcome.kt
@@ -1,8 +1,9 @@
 package com.example.it_supporter.ui.theme.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun WelcomeScreen() {
-
+    Text(text = "Hello")
 }
