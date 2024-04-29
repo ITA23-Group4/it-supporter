cab012822c718224a71f2c42f228f11ca030324f
marcus_rk <makj0005@stud.kea.dk>
2024-04-29 09:50:48 +0200

    added custom color and color obj

--- app/src/main/java/com/example/it_supporter/ui/theme/Color.kt
+++ app/src/main/java/com/example/it_supporter/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object CustomColorScheme {
+    // Light Color Scheme
+    val Orange = Color(0xFFEA620E)
+    val Mocha = Color(0xFFFFEECE)
+    val Beige = Color(0xFFD5C6AC)
+    val OffWhite = Color(0xFFF4F3F1)
+    val LightGray = Color(0xFFFAF9F6)
+    val OffBlack = Color(0xFF212427)
+}
---
+++ app/src/main/java/com/example/it_supporter/ui/theme/components/CustomColumn.kt
@@ -0,0 +1,18 @@
+package com.example.it_supporter.ui.theme.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomColumn(
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier.padding(30.dp, 0.dp)
+    ){
+
+    }
+}
